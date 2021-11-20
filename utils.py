f9eb1b315e7aaa111bcf5d58685488037c5acb53
congzheng <zczlzco@ucl.ac.uk>
2021-11-20 03:10:28 +0000

    The design of multiple verification functions in the untilils file is completed, and two data sets are added to the git resource library.

--- utils.py
+++ utils.py
@@ -7,7 +7,63 @@ def haversine_distance(lat1, lon1, lat2, lon2):
     """
     R = 6371
 
-    d = 2 * R * math.asin( math.sqrt( math.sin((lat2 - lat1)/2) ** 2 + math.cos(lat1) * math.cos(lat2)* (math.sin((lon2 - lon1)/2)**2)) )
+    distance = 2 * R * math.asin( math.sqrt( math.sin((lat2 - lat1)/2) ** 2 + math.cos(lat1) * math.cos(lat2)* (math.sin((lon2 - lon1)/2)**2)) )
 
-    return d
-    raise NotImplementedError
+    return distance
+
+
+def validation_for_id(id):
+
+    if isinstance( id , int ):
+        if len( str( id )) == 5:
+            pass
+        else:
+            raise ValueError( "length of id should be 5" )
+    else:
+        raise TypeError( "id should be a integer" )
+    
+    return True
+
+def validation_for_lat( lat ): 
+
+    if lat > 90 or lat < -90 :
+        raise ValueError(" latitude should be in range of (-90,90) ")
+    else:
+        pass
+
+    return True
+
+def validation_for_lon( lon ): 
+
+    if lon > 180 or lon < -180:
+        raise ValueError(" lontitude should be in range of (-180,180) ")
+    else:
+        pass
+
+    return True
+
+def validation_for_unit( unit ):
+
+    if isinstance(unit, str):
+        if len( unit ) != 2 :
+            raise ValueError("length of unit should be 2")
+        elif unit.isupper() or unit == "99": 
+            pass
+        else:
+            raise ValueError("unit should be capital letters or 99")
+    else:
+        raise ValueError("unit should be a string")
+
+
+    return True
+
+def validation_for_year(year):
+
+    if not isinstance( year, int ):
+        raise ValueError(" year should be a integer ")
+    elif year > 2021:
+        raise ValueError(" year should be less than 2021 ")
+    else: 
+        pass
+
+    return True
